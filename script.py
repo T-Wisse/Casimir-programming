1fade391d37bff9954908dfc9898ae51183290f8
SalehAghajani <S.Aghajani@tudelft.nl>
2021-01-25 14:55:25 +0100

    call functions of test in script

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
-import test.py as test
-
+# import test.py as test
+from test import area_circle
 
 r = 2
-c = test.circumference_circle(r)
 
-print(c)
+# c = test.circumference_circle(r)
+
+print(area_circle(r))
