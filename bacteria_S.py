efde3773ea2157b1ab5081734467fcac7a85a4df
SalehAghajani <S.Aghajani@tudelft.nl>
2021-01-26 11:40:47 +0100

    go to py instead of ipynb

---
+++ bacteria_S.py
@@ -0,0 +1,23 @@
+# This is Saleh
+
+from PIL import Image
+img = Image.open('results_L3.jpg').convert('LA')
+# img.save('greyscale.png')
+
+
+import sys
+import numpy as np
+import skimage.color
+import skimage.filters
+import skimage.io
+import skimage.viewer
+
+# get filename, sigma, and threshold value from command line
+
+#sigma = float(sys.argv[2])
+#t = float(sys.argv[3])
+
+# read and display the original image
+image = skimage.io.imread('results_L3.jpg')
+viewer = skimage.viewer.ImageViewer(image)
+#viewer.show()
