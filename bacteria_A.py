44eed3b1809499283db35558be122b30b7d324d4
Your NameAli_Am git config --listgit config --global user.name Ali_Amgit config --global user.name Your <you@example.comali.amoozandeh@gmail.com>
2021-01-26 14:49:36 +0100

    getting files

--- bacteria_A.py
+++ bacteria_A.py
@@ -1,6 +1,6 @@
 print ("hello Bacteria")
 
-# from PIL import Image
+from PIL import Image
 # img = Image.open('results_L3.jpg').convert('LA')
 # img.save('greyscale.png')
 
@@ -13,19 +13,24 @@ import skimage.viewer
 
 # get filename, sigma, and threshold value from command line
 filename = 'results_L3.jpg'
-sigma = 2
-t = 0.4
+sigma = 1
+t = 0.8
+
+image = skimage.io.imread(fname=filename)
 
 # blur and grayscale before thresholding
 blur = skimage.color.rgb2gray(image)
-blur = skimage.filters.gaussian(blur, sigma=2)
+blur = skimage.filters.gaussian(blur, sigma)
 
 # perform inverse binary thresholding
-mask = blur < t
+t = skimage.filters.threshold_otsu(blur)
+mask = blur > t
 
 # use the mask to select the "interesting" part of the image
 sel = np.zeros_like(image)
 sel[mask] = image[mask]
 
-# save the result
-img.save('mask.png')
+# save binary image; first find beginning of file extension
+dot = filename.index(".")
+binary_file_name = filename[:dot] + "-binary" + filename[dot:]
+skimage.io.imsave(fname=binary_file_name, arr=skimage.img_as_ubyte(sel))
